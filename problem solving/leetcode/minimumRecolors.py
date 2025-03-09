6e7492ae0ae42344dfe57095a845dc2cef512eff
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2025-03-09 10:13:40 +0530

    Update minimumRecolors.py
    optimized approach

--- problem solving/leetcode/minimumRecolors.py
+++ problem solving/leetcode/minimumRecolors.py
@@ -9,4 +9,24 @@ class Solution:
         return operations
             
             
+#O(n) time and O(1) space         
+class Solution:
+    def minimumRecolors2(self, blocks: str, k: int) -> int:
+        res=k
+        recolor=0
+        l=0
+        for  r in range(len(blocks)):
+            if blocks[r]=='W':
+                recolor+=1
+            if r-l+1==k:
+                res=min(res,recolor)
+                if blocks[l]=='W':
+                    recolor-=1
+                l+=1
+            if res==0:
+                break
+        return res
+
+            
+            
         
