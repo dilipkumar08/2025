88ce2df28545b1b37d78986fe3ed336b55d94285
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2025-03-31 13:22:05 +0530

    Update app.py

--- live projects/ml/adult income prediction/app.py
+++ live projects/ml/adult income prediction/app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
-import numpy as np
+from flask import Flask, render_template, request
 import pickle as pkl
-import os
 import pandas as pd
 
 app = Flask(__name__)
