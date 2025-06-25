403d5267499abf766438fe2c65c1824124b99d02
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2025-06-25 17:41:58 +0000

    git diff check

--- yahoo.py
+++ yahoo.py
@@ -1,2 +1,3 @@
 print("yahoooo! I have created a repository using git and writing code in terminal with nano its cool I hated doing stuff in terminal I kinda liking it now \n expect for the complicated stuffs")
 print("Now I am going to add this file without um staging muhaha")
+print("I am going to check the difference btw the code before I added this and after using git diff")
