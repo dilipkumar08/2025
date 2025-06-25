de74eec0d1c5820bf93cd7a09ef21fe6721d92f1
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2025-06-25 17:26:33 +0000

    Commited without staging

--- yahoo.py
+++ yahoo.py
@@ -1,1 +1,2 @@
 print("yahoooo! I have created a repository using git and writing code in terminal with nano its cool I hated doing stuff in terminal I kinda liking it now \n expect for the complicated stuffs")
+print("Now I am going to add this file without um staging muhaha")
