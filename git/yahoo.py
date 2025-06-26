ce2a5f3c2d68b21088f13d4185355771bf32d2b7
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2025-06-26 18:03:48 +0000

    remove git folder

--- git/yahoo.py
+++
@@ -1,1 +0,0 @@
-print("yahoooo! I have created a repository using git and writing code in terminal with nano its cool I hated doing stuff in terminal I kinda liking it now \n expect for the complicated stuffs")
