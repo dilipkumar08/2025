c6f5db1b408bac3b6e4207c65145922d273f39dd
dilipkumar08 <dilipkumar.pdf@gmail.com>
2025-08-19 23:05:08 +0530

    checking which the versions and returning 1 if version1>version2 , -1 if version2>version1 and 0 if both are same

---
+++ problem solving/zoho/bversion_comparison.py
@@ -0,0 +1,49 @@
+
+#function to compare the versions
+def versionComparison(version1:str,version2:str)->bool:
+    #splitting and type casting to compare each partitions
+    version1Partitions=list(map(int,version1.split(".")))
+    version2Partitions=list(map(int,version2.split(".")))
+    version1Length=len(version1Partitions)
+    version2Length=len(version2Partitions)
+    tempIndex=0 #temporary index for comparing partitions
+    print(version1Partitions)
+    print(version2Partitions)
+    while tempIndex<version1Length and tempIndex<version2Length:
+        #version1 > version2
+        if int(version1Partitions[tempIndex])>int(version2Partitions[tempIndex]):
+            return version1
+        #version1 < version2
+        elif int(version1Partitions[tempIndex])<int(version2Partitions[tempIndex]):
+            return version2
+        tempIndex+=1
+    print(tempIndex)
+    #checking if version1 has more partition which are non zer0
+    if tempIndex!=version1Length and tempIndex==version2Length:
+        print("extra length p1")
+        while tempIndex<version1Length:
+            if version1Partitions[tempIndex]!=0:
+                return 1
+            tempIndex+=1
+    
+    #checking if version2 has more partition which are non zer0
+    if tempIndex==version1Length and tempIndex!=version2Length:
+        print("extra length p2")
+        while tempIndex<version2Length:
+            if version2Partitions[tempIndex]!=0:
+                return -1
+            tempIndex+=1
+    
+    #both are same  
+    return 0
+
+
+
+
+
+#main block
+if __name__ == "__main__":
+    version1=str(input("Enter version one: "))
+    version2=str(input("Enter version two: "))
+    #final output
+    print("Result :",versionComparison(version1,version2))
