71a77f0b5d35a2ad652271f9fa31c485f2060ae1
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2025-12-09 09:49:12 +0530

    Update Count_Special_Triplets.py
    added the working one without time limit error

--- problem solving/leetcode/Count_Special_Triplets.py
+++ problem solving/leetcode/Count_Special_Triplets.py
@@ -16,3 +16,29 @@ class Solution:
                     res+= countL*countR
                     
         return res
+
+
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        length=len(nums)
+        if length<3:
+           return 0
+        else:
+            res=0
+            rightCount=dict()
+            leftCount=dict() 
+            for num in nums:
+                rightCount[num]=rightCount.get(num,0)+1
+                leftCount[num]=0
+
+            rightCount[nums[0]]-=1
+            leftCount[nums[0]]=1  
+            
+            for idx in range(1,length-1):
+                target=nums[idx]*2
+                rightCount[nums[idx]]-=1
+                if rightCount.get(target,0)>0 and leftCount.get(target)>0:
+                    res+=rightCount[target]*leftCount[target]
+                leftCount[nums[idx]]=leftCount.get(nums[idx],0)+1
+                
+            return res% (10**9 +7)
