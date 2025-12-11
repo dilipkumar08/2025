bff97e200053d5108f927a37896de9a8e85c7729
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2025-12-11 08:46:09 +0530

    Update Count Covered Buildings.py
    using set but won;t work time limit exceeds

--- problem solving/leetcode/Count Covered Buildings.py
+++ problem solving/leetcode/Count Covered Buildings.py
@@ -27,7 +27,34 @@ class Solution:
                         res+=1
                         break
 
+
+            return res
+                
+# another solution
+class Solution:
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        if n<=2:
+            return 0
+        else:
+            building_set=set()
+            for x,y in buildings:
+                building_set.add((x,y))
+            
+            res=0
+
+
+            for x,y in buildings:
+                left=any((i,y) in building_set for i in range(1,x))
+                right=any((j,y) in building_set for j in range(x+1,n+1))
+                top=any((x,k) in building_set for k in range(y+1,n+1))
+                bottom=any((x,l) in building_set for l in range(1,y))
+
+                if left and right and top and bottom:
+                    res+=1
+
             return res
                 
 
+  
+    
   
